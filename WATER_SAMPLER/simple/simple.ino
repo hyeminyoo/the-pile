f2fcb95ffb3d5d6a6fc36a196a5fed6b72dca8f8
Hyemin Yoo <hyeminyoo@umail.ucsb.edu>
2018-10-10 15:07:20 -0700

    edited simple.ino comments

--- WATER_SAMPLER/simple/simple.ino
+++ WATER_SAMPLER/simple/simple.ino
@@ -1,17 +1,25 @@
+// Steppermotor that runs for 12 hours
+// delay(1+774)=delay(775) milliseconds * 56000 iterations = 12 hours
+// In order to run for:
+// 	12 hours: delay(774);
+// 	24 hours: delay(1547);
+// pin 8: Direction (Low is in, High is out)
+// pin 9: Step (Low is don't step, High is step)
+
 void setup() {
 
 int i;
 
-delay(1800000);
+delay(1800000); //1800000 milliseconds = 30 minutes
 
 digitalWrite(8, LOW);
-//digitalWrite(8, HIGH); // direction (Low is In, High is Out)
+//digitalWrite(8, HIGH);
 
 for (i=0; i<30000; i++) {
-  digitalWrite(9, HIGH); //Step
+  digitalWrite(9, HIGH);
   delay(1);
   digitalWrite(9,LOW);
-  delay(774); ///1547 => 24hours, 774 => 12 hours
+  delay(774);
 }
 
 // Second for loop is because ~ 36000 iterations maxes out the arduino's memory
@@ -20,13 +28,13 @@ for (i=0; i<26000; i++) {
   digitalWrite(9, HIGH); //Step
   delay(1);
   digitalWrite(9,LOW);
-  delay(774); ///1547 => 24hours, 774 => 12 hours
+  delay(774);
 
-} 
+}
 
 }
 
 void loop() {
 
-  
+
 }
